c78dd5c56ebfde926a36ea7ef813aefc4facb7f5
Ric Allinson <capecodehq@yahoo.co.uk>
2012-06-07 23:04:30 -0700

    Bug fix to stop trying to load the same file again even though it was already cached.

--- tests/node_module_tests/util.js
+++ tests/node_module_tests/util.js
@@ -21,7 +21,7 @@
 //    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 //    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-'use strict';
+//'use strict';
 
 var util = require('util');
 
